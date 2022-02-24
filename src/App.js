ade1c0d1f7a281a9cd47a46d9b90ce40773199da
dsow09 <djibril.sow9717@gmail.com>
2022-02-24 17:19:44 +0000

    Add checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ const [data, setData] = useState([]);// where to store the returned data
+ const [error, setError] = useState(null);// where to store the coming errors
 
+ axios.get("https://jsonplaceholder.typicode.com/users")
+ .then(function(response) {
+   setData(response.data);
+   console.log(response.data)
+ })
+ .catch(function(error) {
+   setError(error);
+ })
+ 
+ return (
+   <div>
+      <table className="table  table-bordered">
+        <thead>
+          <tr class="table-active">
+              <th scope="col">Nom</th>
+              <th scope="col">Username</th>
+              <th scope="col">Email</th>
+              <th scope="col">Téléphone</th>
+              <th scope="col">Adresse</th>
+          </tr>
+        </thead>
+      {data.map(user => (
+            <tbody key={user.id}>
+              <tr>
+                <th scope="row">{user.name}</th>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+                <td>{user.phone}</td>
+                <td>{user.address.street}</td>
+              </tr>
+            </tbody>
+       ))}
+      </table>
+   </div>
+ )
+};
 export default App;
